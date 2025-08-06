693b9dcc901b0e722b4bacb581ea32dd5d77353b
minnettar <todo@sekeroglugroup.com>
2025-08-06 16:45:36 +0300

    Update crm.py

--- crm.py
+++ crm.py
@@ -14,14 +14,25 @@ from gspread_dataframe import get_as_dataframe, set_with_dataframe
 import json
 import os
 
-# Streamlit secrets'ten oku
-service_account_info = st.secrets["SERVICE_ACCOUNT_JSON"]
-if isinstance(service_account_info, str):
-    service_account_info = json.loads(service_account_info)
+# Streamlit secrets'ten bilgileri oku ve dosyaya yaz
+service_account_info = {
+    "type": st.secrets["type"],
+    "project_id": st.secrets["project_id"],
+    "private_key_id": st.secrets["private_key_id"],
+    "private_key": st.secrets["private_key"],
+    "client_email": st.secrets["client_email"],
+    "client_id": st.secrets["client_id"],
+    "auth_uri": st.secrets["auth_uri"],
+    "token_uri": st.secrets["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["client_x509_cert_url"],
+    "universe_domain": st.secrets["universe_domain"],
+}
 
-# JSON'u dosya gibi kullanmak için:
 with open("service_account.json", "w") as f:
     json.dump(service_account_info, f)
+
+
 
 
 st.set_page_config(page_title="ŞEKEROĞLU İHRACAT CRM", layout="wide")
