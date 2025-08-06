b1e6ca42f9edea275e1b4825d0cf7acbfe3777bf
minnettar <todo@sekeroglugroup.com>
2025-08-06 17:07:57 +0300

    Update crm.py

--- crm.py
+++ crm.py
@@ -14,23 +14,14 @@ from gspread_dataframe import get_as_dataframe, set_with_dataframe
 import json
 import os
 
-# Streamlit secrets'ten bilgileri oku ve dosyaya yaz
-service_account_info = {
-    "type": st.secrets["type"],
-    "project_id": st.secrets["project_id"],
-    "private_key_id": st.secrets["private_key_id"],
-    "private_key": st.secrets["private_key"],
-    "client_email": st.secrets["client_email"],
-    "client_id": st.secrets["client_id"],
-    "auth_uri": st.secrets["auth_uri"],
-    "token_uri": st.secrets["token_uri"],
-    "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
-    "client_x509_cert_url": st.secrets["client_x509_cert_url"],
-    "universe_domain": st.secrets["universe_domain"],
-}
+SCOPES = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
 
-with open("service_account.json", "w") as f:
-    json.dump(service_account_info, f)
+credentials = Credentials.from_service_account_info(st.secrets, scopes=SCOPES)
+gc = gspread.authorize(credentials)
 
 
 
