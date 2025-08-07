9f018b996a9011f477e287568bf1ddfe624bf10c
minnettar <todo@sekeroglugroup.com>
2025-08-07 23:33:45 +0300

    Update crm.py

--- crm.py
+++ crm.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import gspread
 from google.oauth2.service_account import Credentials
-from datetime import datetime
+import datetime
 from googleapiclient.discovery import build
 import os
 import smtplib
