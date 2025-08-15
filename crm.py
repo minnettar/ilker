414075309a3256cf10e24bdad7a1d3e7e6c5c7f6
minnettar <todo@sekeroglugroup.com>
2025-08-15 10:24:11 +0300

    Update crm.py

--- crm.py
+++ crm.py
@@ -24,7 +24,7 @@ import pandas as pd
 import pandas as _pd
 
 
-SHEET_ID, Union= "1A_gL11UL6JFAoZrMrg92K8bAegeCn_KzwUyU8AWzE_0"
+SHEET_ID = "1A_gL11UL6JFAoZrMrg92K8bAegeCn_KzwUyU8AWzE_0"
 
 # =============================
 # === CRM ILKER: Revizyon 1 ===
