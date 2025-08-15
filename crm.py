1ae3f228a144c7485e76632e1dbf5e43b9a4bdc2
minnettar <todo@sekeroglugroup.com>
2025-08-15 10:23:49 +0300

    Update crm.py

--- crm.py
+++ crm.py
@@ -19,10 +19,12 @@ from oauth2client.service_account import ServiceAccountCredentials
 from pydrive2.auth import GoogleAuth
 from pydrive2.drive import GoogleDrive
 from email.message import EmailMessage
-from typing import Any, Optional, Union, Dict, List, Tuple
+from typing import Any, Dict, List, Optional, Tuple, Tuple
+import pandas as pd
+import pandas as _pd
 
 
-SHEET_ID = "1A_gL11UL6JFAoZrMrg92K8bAegeCn_KzwUyU8AWzE_0"
+SHEET_ID, Union= "1A_gL11UL6JFAoZrMrg92K8bAegeCn_KzwUyU8AWzE_0"
 
 # =============================
 # === CRM ILKER: Revizyon 1 ===
