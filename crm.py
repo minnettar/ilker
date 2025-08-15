c206792452e26183296e3392202e26c043acff58
minnettar <todo@sekeroglugroup.com>
2025-08-15 11:53:13 +0300

    Update crm.py

--- crm.py
+++ crm.py
@@ -410,7 +410,7 @@ if "menu_state" not in st.session_state:
 # 5) CSS (radio’yu kart gibi; input’u gizlemiyoruz)
 st.sidebar.markdown("""
 <style>
-section[data-testid="stSidebar"] { padding-top: 0.5rem; }
+div[data-testid="stSidebar"] { padding-top: 0.5rem; }
 div[data-testid="stSidebar"] .stRadio > div { gap: 10px !important; }
 div[data-testid="stSidebar"] .stRadio label {
     border-radius: 12px;
